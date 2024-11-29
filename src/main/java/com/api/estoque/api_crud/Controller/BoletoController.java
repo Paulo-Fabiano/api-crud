fe696d2f00b93fdbd181abcfbfede16db614a188
paulo <pfabianof@gmail.com>
2024-11-28 22:58:57 -0300

    Atualização das rotas do BOLETO

--- src/main/java/com/api/estoque/api_crud/Controller/BoletoController.java
+++ src/main/java/com/api/estoque/api_crud/Controller/BoletoController.java
@@ -1,23 +1,52 @@
 package com.api.estoque.api_crud.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.api.estoque.api_crud.Entity.Boleto;
+import com.api.estoque.api_crud.Exceptions.BoletoNaoAdicionado;
+import com.api.estoque.api_crud.Exceptions.BoletoNaoEncontrado;
+import com.api.estoque.api_crud.Service.BoletoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
 @RequestMapping("/boleto")
 public class BoletoController {
 
+    @Autowired
+    private BoletoService boletoService;
+
     @GetMapping
-    public ModelAndView viewListaBoleto() {
+    public String viewListaBoleto(Model model) {
         ModelAndView mv = new ModelAndView("/boleto/listaBoletos");
-        return mv;
+        List<Boleto> boletos = new ArrayList<>();
+        try {
+            boletos = boletoService.listarBoleto();
+        } catch (BoletoNaoEncontrado e) {
+            throw new RuntimeException(e);
+        }
+        model.addAttribute("boletos", boletos);
+        return "redirect:/boleto";
     }
 
     @GetMapping("/adicionar")
-    public ModelAndView viweBoletoAdicioanar() {
+    public ModelAndView viewBoletoAdicioanar() {
         ModelAndView mv = new ModelAndView("/boleto/addBoleto");
         return mv;
+    }
+
+    @PostMapping("/api/salvar")
+    public String viewAddBoleto(@ModelAttribute  Boleto boleto) {
+        ModelAndView mv = new ModelAndView("/boleto/addBoleto");
+        try {
+            boletoService.adicinarBoleto(boleto);
+        } catch (BoletoNaoAdicionado e) {
+            throw new RuntimeException(e);
+        }
+        return "redirect:/boleto/adicionar";
     }
 }
